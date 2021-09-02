2dbde7c394b29dd770ab8bd4537d3798f4bcdb88
Jyothy Baby <jyothy.v.baby@gmail.com>
2021-09-01 23:44:49 -0400

    Committing starter code in server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+//Importing the requirements
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3001;
+//Invoke app.use() and serve static files from the '/public' folder
+app.use(express.static("public"));
+
+
+
+
+
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
